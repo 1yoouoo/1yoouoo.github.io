f66afb271461d75874b121b1fb46881694f226a5
blanc <1yoouoo@gmail.com>
2023-03-29 01:51:27 +0000

    publish blog

--- publish.sh
+++ publish.sh
@@ -1,11 +1,12 @@
 # active python
 echo "started"
 cd /home/yoon/blog/1yoouoo.github.io
-/usr/bin/python3 main.py
+/usr/bin/python3 main.py >> /home/yoon/blog/1yoouoo.github.io/main.log 2>&1 
 
 git pull
 git config --local user.name "blanc"
 git config --local user.email "1yoouoo@gmail.com" 
+sleep 0.1
 git add -A
 git commit -m "publish blog"
 git push origin HEAD
